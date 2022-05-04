48c8febddc03bbc726cd6314d7932691a1a03e2d
little_jack_2016 <860681233@qq.com>
2022-05-05 01:06:19 +0800

    增加自动刷新，以及 默认无法加载 bootstrap.properties文件的依赖。

--- shoppingmall-coupon/src/main/java/com/cjr/shoppingmall/coupon/controller/CouponController.java
+++ shoppingmall-coupon/src/main/java/com/cjr/shoppingmall/coupon/controller/CouponController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import com.cjr.common.utils.R;
  * @email chenjunran22@gmail.com
  * @date 2022-05-02 15:09:42
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
