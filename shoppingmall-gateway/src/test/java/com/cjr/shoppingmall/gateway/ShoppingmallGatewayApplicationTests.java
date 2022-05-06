4612ceed580a8ad3bd255e7fb7b0f3caa383ccda
little_jack_2014 <860681233@qq.com>
2022-05-06 17:37:03 +0800

    新增 Gateway 网关模块

---
+++ shoppingmall-gateway/src/test/java/com/cjr/shoppingmall/gateway/ShoppingmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjr.shoppingmall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
